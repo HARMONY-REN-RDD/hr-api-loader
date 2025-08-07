d9a718383d74104d243ea22fec5cbfb9f2a29ee5
kai5041 <kai5041contact@gmail.com>
2025-08-07 13:12:24 +0200

    Tweaks

---
+++ src/api/1/test_section/test.ts
@@ -0,0 +1,3 @@
+export default function test() {
+  return 'test from func v1 section test_section';
+}
--- src/index.ts
+++ src/index.ts
@@ -16,25 +16,17 @@ class ApiLoader {
 
   constructor() {
     const api_path_arg = process.argv[1];
-    if (!api_path_arg) {
-      throw new Error("Script path (argv[1]) is undefined");
-    }
-
+    if (!api_path_arg) throw new Error("Script path (argv[1]) is undefined");
     this.api_path = path.join(path.dirname(api_path_arg), 'api');
+    if (!fs.existsSync(this.api_path)) throw new Error(`API folder does not exist at path: ${this.api_path}`);
+  }
 
-    if (!fs.existsSync(this.api_path)) {
-      throw new Error(`API folder does not exist at path: ${this.api_path}`);
-    }
-
-    // Initialize version keys
-    for (const version of fs.readdirSync(this.api_path)) {
+  async init() {
+    const versions = fs.readdirSync(this.api_path);
+    for (const version of versions) {
       this.methods[version] = {};
-    }
-
-    // For each version, load methods recursively
-    for (const version of Object.keys(this.methods)) {
       const version_path = path.join(this.api_path, version);
-      this.load_methods_recursively(version_path, this.methods[version]!, []);
+      await this.load_methods_recursively(version_path, this.methods[version], []);
     }
   }
 
@@ -42,31 +34,25 @@ class ApiLoader {
     for (const entry of fs.readdirSync(dir_path)) {
       const full_path = path.join(dir_path, entry);
       const stat = fs.statSync(full_path);
-
       if (stat.isDirectory()) {
-        // Recurse into subdirectory with updated path_parts
-        this.load_methods_recursively(full_path, method_map, [...path_parts, entry]);
-      } else if (stat.isFile() && entry.endsWith('.js') ) {
+        await this.load_methods_recursively(full_path, method_map, [...path_parts, entry]);
+      } else if (stat.isFile() && entry.endsWith('.js')) {
         const filename = path.parse(entry).name;
-
-        // Combine path parts and filename with dashes
         const method_key = [...path_parts, filename].join('-');
-        const mod = await import(full_path);
-        const fn = mod.default || mod;
-        method_map[method_key] = fn;
+        try {
+          const mod = await import(full_path);
+          const fn = mod.default || mod;
+          method_map[method_key] = fn;
+        } catch (e) {
+          console.error(`Failed to load method ${method_key} from ${full_path}`, e);
+        }
       }
     }
   }
 
-  call(version: string, method: string, ...args: any[]): any {
-    if (!this.methods[version]) {
-      throw new VersionNotSupportedError(`Version ${version} is not supported`);
-    }
-
-    if (!this.methods[version][method]) {
-      throw new MethodNotFoundError(`Method ${method} is not found in version ${version}`);
-    }
-
+  call(version: string, method: string, ...args: any[]) {
+    if (!this.methods[version]) throw new VersionNotSupportedError(`Version ${version} is not supported`);
+    if (!this.methods[version][method]) throw new MethodNotFoundError(`Method ${method} not found in version ${version}`);
     return this.methods[version][method](...args);
   }
 }
@@ -75,7 +61,8 @@ export default ApiLoader;
 
 async function main() {
   const loader = new ApiLoader();
+  await loader.init();
   console.log(loader.methods);
 }
 
-// main();
+main();
