ad3c0083da2c532f1d79bb19696965638fb70702
kai5041 <kai5041contact@gmail.com>
2025-08-10 23:00:34 +0200

    Bug hunting

--- src/index.ts
+++ src/index.ts
@@ -40,7 +40,7 @@ class ApiLoader {
         const filename = path.parse(entry).name;
         const method_key = [...path_parts, filename].join('-');
         try {
-          const mod = require(full_path);
+          const mod = await import(full_path);
           const fn = mod.default || mod;
           method_map[method_key] = fn;
         } catch (e) {
